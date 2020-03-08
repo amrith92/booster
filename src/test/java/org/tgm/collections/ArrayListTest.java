807f2e59c9d81d06cebb5006d781df9ac4d02adc
Amrith Nayak <shroff.amrith@flipkart.com>
2020-03-08 19:22:14 +0530

    - Added test case to support index-based remove

--- src/test/java/org/tgm/collections/ArrayListTest.java
+++ src/test/java/org/tgm/collections/ArrayListTest.java
@@ -22,4 +22,15 @@ public class ArrayListTest {
         Assert.assertNotNull(arrayListUnderTest.get(0));
         Assert.assertEquals(5, (int) arrayListUnderTest.get(0));
     }
+
+    @Test
+    public void shouldSupportIndexBasedRemove() {
+
+        arrayListUnderTest.add(1);
+
+        Assert.assertEquals(1, arrayListUnderTest.size());
+
+        arrayListUnderTest.remove(0);
+        Assert.assertEquals(0, arrayListUnderTest.size());
+    }
 }
